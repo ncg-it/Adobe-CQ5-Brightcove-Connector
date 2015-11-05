38bfdc2becd93f58f5cd60407a1f349c10c04c03
Yan Kisen <yan.kisen@coresecure.com>
2015-11-05 14:41:16 -0500

    clean clientlibs

--- current/brightcove-view/src/main/content/jcr_root/etc/designs/cs/brightcove/brightcoveExperiences/js/BrightcoveExperiences.js
+++ current/brightcove-view/src/main/content/jcr_root/etc/designs/cs/brightcove/brightcoveExperiences/js/BrightcoveExperiences.js
@@ -24,10 +24,11 @@
 
 
  */
-var BCLplayer;
-var BCLexperienceModule;
-var BCLvideoPlayer;
-var BCLcurrentVideo;
+
+var BCLplayer,
+    BCLexperienceModule,
+    BCLvideoPlayer,
+    BCLcurrentVideo;
 
 //listener for player error
 function onPlayerError(event) {
@@ -36,26 +37,26 @@ function onPlayerError(event) {
 
 //listener for when player is loaded
 function onPlayerLoaded(id) {
-  // newLog();
+    // newLog();
 //  log("EVENT: onPlayerLoaded");
-  BCLplayer = brightcove.getExperience(id);
-  if (typeof BCLplayer!='undefined') BCLexperienceModule = BCLplayer.getModule(APIModules.EXPERIENCE);
+    BCLplayer = brightcove.getExperience(id);
+    if (typeof BCLplayer != 'undefined') BCLexperienceModule = BCLplayer.getModule(APIModules.EXPERIENCE);
 }
 
 //listener for when player is ready
 function onPlayerReady(event) {
- // log("EVENT: onPlayerReady");
+    // log("EVENT: onPlayerReady");
 
-  // get a reference to the video player module
-  BCLvideoPlayer = BCLplayer.getModule(APIModules.VIDEO_PLAYER);
-  // add a listener for media change events
-  BCLvideoPlayer.addEventListener(BCMediaEvent.BEGIN, onMediaBegin);
-  BCLvideoPlayer.addEventListener(BCMediaEvent.COMPLETE, onMediaBegin);
-  BCLvideoPlayer.addEventListener(BCMediaEvent.CHANGE, onMediaBegin);
-  BCLvideoPlayer.addEventListener(BCMediaEvent.ERROR, onMediaBegin);
-  BCLvideoPlayer.addEventListener(BCMediaEvent.PLAY, onMediaBegin);
-  BCLvideoPlayer.addEventListener(BCMediaEvent.STOP, onMediaBegin);
-}  
+    // get a reference to the video player module
+    BCLvideoPlayer = BCLplayer.getModule(APIModules.VIDEO_PLAYER);
+    // add a listener for media change events
+    BCLvideoPlayer.addEventListener(BCMediaEvent.BEGIN, onMediaBegin);
+    BCLvideoPlayer.addEventListener(BCMediaEvent.COMPLETE, onMediaBegin);
+    BCLvideoPlayer.addEventListener(BCMediaEvent.CHANGE, onMediaBegin);
+    BCLvideoPlayer.addEventListener(BCMediaEvent.ERROR, onMediaBegin);
+    BCLvideoPlayer.addEventListener(BCMediaEvent.PLAY, onMediaBegin);
+    BCLvideoPlayer.addEventListener(BCMediaEvent.STOP, onMediaBegin);
+}
 //listener for media change events
 function onMediaBegin(event) {
     var BCLcurrentVideoID;
@@ -64,25 +65,25 @@ function onMediaBegin(event) {
     BCLcurrentVideoNAME = BCLvideoPlayer.getCurrentVideo().displayName;
     switch (event.type) {
         case "mediaBegin":
-            var currentVideoLength ="0";
+            var currentVideoLength = "0";
             currentVideoLength = BCLvideoPlayer.getCurrentVideo().length;
-            if (currentVideoLength != "0") currentVideoLength = currentVideoLength/1000;
-            if (typeof _gaq != "undefined") _gaq.push(['_trackEvent', location.pathname, event.type+" - "+currentVideoLength, BCLcurrentVideoNAME+" - "+BCLcurrentVideoID]);
+            if (currentVideoLength != "0") currentVideoLength = currentVideoLength / 1000;
+            if (typeof _gaq != "undefined") _gaq.push(['_trackEvent', location.pathname, event.type + " - " + currentVideoLength, BCLcurrentVideoNAME + " - " + BCLcurrentVideoID]);
             break;
         case "mediaPlay":
-            _gaq.push(['_trackEvent', location.pathname, event.type+" - "+event.position, BCLcurrentVideoNAME+" - "+BCLcurrentVideoID]);
+            _gaq.push(['_trackEvent', location.pathname, event.type + " - " + event.position, BCLcurrentVideoNAME + " - " + BCLcurrentVideoID]);
             break;
         case "mediaStop":
-            _gaq.push(['_trackEvent', location.pathname, event.type+" - "+event.position, BCLcurrentVideoNAME+" - "+BCLcurrentVideoID]);
+            _gaq.push(['_trackEvent', location.pathname, event.type + " - " + event.position, BCLcurrentVideoNAME + " - " + BCLcurrentVideoID]);
             break;
         case "mediaChange":
-            _gaq.push(['_trackEvent', location.pathname, event.type+" - "+event.position, BCLcurrentVideoNAME+" - "+BCLcurrentVideoID]);
+            _gaq.push(['_trackEvent', location.pathname, event.type + " - " + event.position, BCLcurrentVideoNAME + " - " + BCLcurrentVideoID]);
             break;
         case "mediaComplete":
-            _gaq.push(['_trackEvent', location.pathname, event.type+" - "+event.position, BCLcurrentVideoNAME+" - "+BCLcurrentVideoID]);
+            _gaq.push(['_trackEvent', location.pathname, event.type + " - " + event.position, BCLcurrentVideoNAME + " - " + BCLcurrentVideoID]);
             break;
         default:
-            _gaq.push(['_trackEvent', location.pathname, event.type, BCLcurrentVideoNAME+" - "+BCLcurrentVideoID]);
+            _gaq.push(['_trackEvent', location.pathname, event.type, BCLcurrentVideoNAME + " - " + BCLcurrentVideoID]);
     }
 }
 
@@ -95,86 +96,86 @@ if (customBC == undefined) {
             return document.createElement(el);
         }
     };
-    customBC.createVideo = function (width,height,playerID,playerKey,videoPlayer,VideoRandomID) {
-    	var innerhtml = '<object id="myExperience_'+VideoRandomID+'" class="BrightcoveExperience">';
-		innerhtml += '<param name="bgcolor" value="#FFFFFF" />';
-		innerhtml += '<param name="width" value="'+width+'" />';
-		innerhtml += '<param name="height" value="'+height+'" />';
-		innerhtml += '<param name="playerID" value="'+playerID+'" />';
-		innerhtml += '<param name="playerKey" value="'+playerKey+'" />';
-		innerhtml += '<param name="isVid" value="true" />';
-		innerhtml += '<param name="isUI" value="true" />';
-		innerhtml += '<param name="dynamicStreaming" value="true" />';
-		innerhtml += '<param name="@videoPlayer" value="'+videoPlayer+'" />';
-		if ( window.location.protocol == 'https:') innerhtml += '<param name="secureConnections" value="true" /> ';
-		innerhtml += '<param name="templateLoadHandler" value="onPlayerLoaded" />';
-		innerhtml += '<param name="templateReadyHandler" value="onPlayerReady" />';
-		innerhtml += '<param name="templateErrorHandler" value="onPlayerError" />';
-		innerhtml += '<param name="includeAPI" value="true" /> ';
-		innerhtml += '<param name="wmode" value="transparent" />';
-		innerhtml += '</object>';
-		var objID = document.getElementById(VideoRandomID);
-		objID.innerHTML = innerhtml;
-		
-		var apiInclude = customBC.createElement('script');
-		apiInclude.type = "text/javascript";
-		apiInclude.src = "https://sadmin.brightcove.com/js/BrightcoveExperiences.js";
-		objID.parentNode.appendChild(apiInclude);
-		
-		apiInclude = customBC.createElement('script');
-		apiInclude.type = "text/javascript";
-		apiInclude.src = "https://sadmin.brightcove.com/js/APIModules_all.js";
-		objID.parentNode.appendChild(apiInclude);
-		
-		apiInclude = customBC.createElement('script');
-		apiInclude.type = "text/javascript";
-		apiInclude.src = "https://files.brightcove.com/bc-mapi.js";
-		objID.parentNode.appendChild(apiInclude);
-		
-		apiInclude = customBC.createElement('script');
-		apiInclude.type = "text/javascript";
-		apiInclude.text  = "window.onload = function() {brightcove.createExperiences();};";
-		objID.parentNode.appendChild(apiInclude);
+    customBC.createVideo = function (width, height, playerID, playerKey, videoPlayer, VideoRandomID) {
+        var innerhtml = '<object id="myExperience_' + VideoRandomID + '" class="BrightcoveExperience">';
+        innerhtml += '<param name="bgcolor" value="#FFFFFF" />';
+        innerhtml += '<param name="width" value="' + width + '" />';
+        innerhtml += '<param name="height" value="' + height + '" />';
+        innerhtml += '<param name="playerID" value="' + playerID + '" />';
+        innerhtml += '<param name="playerKey" value="' + playerKey + '" />';
+        innerhtml += '<param name="isVid" value="true" />';
+        innerhtml += '<param name="isUI" value="true" />';
+        innerhtml += '<param name="dynamicStreaming" value="true" />';
+        innerhtml += '<param name="@videoPlayer" value="' + videoPlayer + '" />';
+        if (window.location.protocol == 'https:') innerhtml += '<param name="secureConnections" value="true" /> ';
+        innerhtml += '<param name="templateLoadHandler" value="onPlayerLoaded" />';
+        innerhtml += '<param name="templateReadyHandler" value="onPlayerReady" />';
+        innerhtml += '<param name="templateErrorHandler" value="onPlayerError" />';
+        innerhtml += '<param name="includeAPI" value="true" /> ';
+        innerhtml += '<param name="wmode" value="transparent" />';
+        innerhtml += '</object>';
+        var objID = document.getElementById(VideoRandomID);
+        objID.innerHTML = innerhtml;
+
+        var apiInclude = customBC.createElement('script');
+        apiInclude.type = "text/javascript";
+        apiInclude.src = "https://sadmin.brightcove.com/js/BrightcoveExperiences.js";
+        objID.parentNode.appendChild(apiInclude);
+
+        apiInclude = customBC.createElement('script');
+        apiInclude.type = "text/javascript";
+        apiInclude.src = "https://sadmin.brightcove.com/js/APIModules_all.js";
+        objID.parentNode.appendChild(apiInclude);
+
+        apiInclude = customBC.createElement('script');
+        apiInclude.type = "text/javascript";
+        apiInclude.src = "https://files.brightcove.com/bc-mapi.js";
+        objID.parentNode.appendChild(apiInclude);
+
+        apiInclude = customBC.createElement('script');
+        apiInclude.type = "text/javascript";
+        apiInclude.text = "window.onload = function() {brightcove.createExperiences();};";
+        objID.parentNode.appendChild(apiInclude);
     };
-    customBC.createPlaylist = function (width,height,playerID,playerKey,videoPlayer,VideoRandomID) {
-    	var innerhtml = '<object id="myExperience_'+VideoRandomID+'" class="BrightcoveExperience">';
-		innerhtml += '<param name="bgcolor" value="#FFFFFF" />';
-		innerhtml += '<param name="width" value="'+width+'" />';
-		innerhtml += '<param name="height" value="'+height+'" />';
-		innerhtml += '<param name="playerID" value="'+playerID+'" />';
-		innerhtml += '<param name="playerKey" value="'+playerKey+'" />';
-		innerhtml += '<param name="isVid" value="true" />';
-		innerhtml += '<param name="isUI" value="true" />';
-		innerhtml += '<param name="dynamicStreaming" value="true" />';
-		innerhtml += '<param name="@playlistTabs" value="'+videoPlayer+'" />';
-		if ( window.location.protocol == 'https:') innerhtml += '<param name="secureConnections" value="true" /> ';
-		innerhtml += '<param name="templateLoadHandler" value="onPlayerLoaded" />';
-		innerhtml += '<param name="templateReadyHandler" value="onPlayerReady" />';
-		innerhtml += '<param name="templateErrorHandler" value="onPlayerError" />';
-		innerhtml += '<param name="includeAPI" value="true" /> ';
-		innerhtml += '<param name="wmode" value="transparent" />';
-		innerhtml += '</object>';
-		var objID = document.getElementById(VideoRandomID);
-		objID.innerHTML = innerhtml;
-		
-		var apiInclude = customBC.createElement('script');
-		apiInclude.type = "text/javascript";
-		apiInclude.src = "https://sadmin.brightcove.com/js/BrightcoveExperiences.js";
-		objID.parentNode.appendChild(apiInclude);
-		
-		apiInclude = customBC.createElement('script');
-		apiInclude.type = "text/javascript";
-		apiInclude.src = "https://sadmin.brightcove.com/js/APIModules_all.js";
-		objID.parentNode.appendChild(apiInclude);
-		
-		apiInclude = customBC.createElement('script');
-		apiInclude.type = "text/javascript";
-		apiInclude.src = "https://files.brightcove.com/bc-mapi.js";
-		objID.parentNode.appendChild(apiInclude);
-		
-		apiInclude = customBC.createElement('script');
-		apiInclude.type = "text/javascript";
-		apiInclude.text  = "window.onload = function() {brightcove.createExperiences();};";
-		objID.parentNode.appendChild(apiInclude);
+    customBC.createPlaylist = function (width, height, playerID, playerKey, videoPlayer, VideoRandomID) {
+        var innerhtml = '<object id="myExperience_' + VideoRandomID + '" class="BrightcoveExperience">';
+        innerhtml += '<param name="bgcolor" value="#FFFFFF" />';
+        innerhtml += '<param name="width" value="' + width + '" />';
+        innerhtml += '<param name="height" value="' + height + '" />';
+        innerhtml += '<param name="playerID" value="' + playerID + '" />';
+        innerhtml += '<param name="playerKey" value="' + playerKey + '" />';
+        innerhtml += '<param name="isVid" value="true" />';
+        innerhtml += '<param name="isUI" value="true" />';
+        innerhtml += '<param name="dynamicStreaming" value="true" />';
+        innerhtml += '<param name="@playlistTabs" value="' + videoPlayer + '" />';
+        if (window.location.protocol == 'https:') innerhtml += '<param name="secureConnections" value="true" /> ';
+        innerhtml += '<param name="templateLoadHandler" value="onPlayerLoaded" />';
+        innerhtml += '<param name="templateReadyHandler" value="onPlayerReady" />';
+        innerhtml += '<param name="templateErrorHandler" value="onPlayerError" />';
+        innerhtml += '<param name="includeAPI" value="true" /> ';
+        innerhtml += '<param name="wmode" value="transparent" />';
+        innerhtml += '</object>';
+        var objID = document.getElementById(VideoRandomID);
+        objID.innerHTML = innerhtml;
+
+        var apiInclude = customBC.createElement('script');
+        apiInclude.type = "text/javascript";
+        apiInclude.src = "https://sadmin.brightcove.com/js/BrightcoveExperiences.js";
+        objID.parentNode.appendChild(apiInclude);
+
+        apiInclude = customBC.createElement('script');
+        apiInclude.type = "text/javascript";
+        apiInclude.src = "https://sadmin.brightcove.com/js/APIModules_all.js";
+        objID.parentNode.appendChild(apiInclude);
+
+        apiInclude = customBC.createElement('script');
+        apiInclude.type = "text/javascript";
+        apiInclude.src = "https://files.brightcove.com/bc-mapi.js";
+        objID.parentNode.appendChild(apiInclude);
+
+        apiInclude = customBC.createElement('script');
+        apiInclude.type = "text/javascript";
+        apiInclude.text = "window.onload = function() {brightcove.createExperiences();};";
+        objID.parentNode.appendChild(apiInclude);
     };
 }
